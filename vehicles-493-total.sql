0da0db32821cd5e60dcc5671758ee66093068c41
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2025-11-15 16:42:28 +1100

    Add files via upload

--- vehicles-493-total.sql
+++ vehicles-493-total.sql
@@ -1,24 +1,3 @@
--- --------------------------------------------------------
--- Host:                         192.168.0.21
--- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             12.5.0.6677
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Dumping database structure for test data
-CREATE DATABASE IF NOT EXISTS `test data` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `test data`;
-
 -- Dumping structure for table test data.vehicles
 CREATE TABLE IF NOT EXISTS `vehicles` (
   `name` varchar(60) NOT NULL,
@@ -522,9 +501,3 @@ INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES
 	('Dominator FX', 'dominator10', 113850, 'muscle'),
 	('Impaler SZ', 'impaler5', 63550, 'muscle'),
 	('Vigero ZX Convertible', 'vigero3', 140500, 'muscle');
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
